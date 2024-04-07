5e98c16490913fc4868afd7b034729e46fae9f37
Souradeep Nanda <souradeep.nanda123@gmail.com>
2024-04-06 20:03:18 -0500

    Exelify for summary tasks

--- exelify_results.py
+++ exelify_results.py
@@ -1,6 +1,9 @@
+import json
 import os
 import pandas as pd
 from constants import TASK_NAMES, SELECTIVE_ANNOTATION_METHODS
+
+OUTPUTS_DIR = "outputs"
 
 
 # Function to parse the accuracy from a result_summary.txt file
@@ -12,21 +15,37 @@ def parse_accuracy(file_path):
 
 
 rows = []
+soft_rows = []
 # Iterate over every combination of task and method
-for task in TASK_NAMES:
-    for method in SELECTIVE_ANNOTATION_METHODS:
-        file_path = os.path.join("outputs", task, method, "result_summary.txt")
-        if os.path.exists(file_path):
-            accuracy = parse_accuracy(file_path)
-            rows.append({"task": task, "method": method, "accuracy": accuracy})
-        else:
-            print(f"File not found: {file_path}")
+for model_name in os.listdir(OUTPUTS_DIR):
+    for task in TASK_NAMES:
+        for method in SELECTIVE_ANNOTATION_METHODS:
+            file_path = os.path.join(
+                OUTPUTS_DIR, model_name, task, method, "result_summary.txt"
+            )
+            json_file_path = file_path.replace(".txt", ".json")
+            metadata = {
+                "model_name": model_name,
+                "task": task,
+                "method": method,
+            }
+
+            if os.path.exists(file_path):
+                accuracy = parse_accuracy(file_path)
+                rows.append({**metadata, "accuracy": accuracy})
+            elif os.path.exists(json_file_path):
+                with open(json_file_path) as f:
+                    results = json.load(f)
+                soft_rows.append({**metadata, **results})
+            else:
+                # print(f"File not found: {file_path}")
+                ...
 
 # Convert the list of dictionaries into a DataFrame
 df = pd.DataFrame(rows)
 
 # Pivot the DataFrame
-pivot_df = df.pivot(index="method", columns="task", values="accuracy")
+pivot_df = df.pivot(index=["model_name", "method"], columns="task", values="accuracy")
 
 # Add an 'average' column which is the row average
 pivot_df["average"] = pivot_df.mean(axis=1)
@@ -35,6 +54,15 @@ pivot_df["average"] = pivot_df.mean(axis=1)
 pivot_df_sorted = pivot_df.sort_values(by="average")
 
 # Save the sorted pivoted DataFrame to a CSV file
-pivot_df_sorted.to_csv("results_summary.csv")
+file_name = f"results_hard_summary.csv"
+pivot_df_sorted.to_csv(file_name)
 
-print("Sorted CSV file has been created as 'results_summary.csv'.")
+print(f"Sorted CSV file has been created as {file_name}.")
+
+soft_df = pd.DataFrame(soft_rows)
+# metrics = ["rouge1", "rouge2", "rougeL", "rougeLsum", "accuracy"]
+
+file_name = f"soft_results_summary.csv"
+soft_df.to_csv(f"results_soft_summary.csv", index=False)
+
+print(f"Sorted CSV file has been created as {file_name}.")
